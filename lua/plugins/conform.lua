f7aec98626b54996097728468203a043df1e972b
Jesse Armand <jesse.armand@screeningeagle.com>
2025-12-10 15:17:36 +0800

    chore(conform): Set xml formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,15 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            javascript = { "biome" },
-            typescript = { "biome" },
-            javascriptreact = { "biome" },
-            typescriptreact = { "biome" },
-            json = { "biome" },
-            html = { "prettier" },
-            fish = {}, -- disable fish_indent if you don't use it
-        },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "biome" },
+      typescript = { "biome" },
+      javascriptreact = { "biome" },
+      typescriptreact = { "biome" },
+      json = { "biome" },
+      html = { "prettier" },
+      xml = { "xmllint" },
+      fish = {}, -- disable fish_indent if you don't use it
     },
+  },
 }
