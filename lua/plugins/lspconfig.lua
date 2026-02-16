63505bc3f2d093af87c7382521a0c91bd5f81eb2
Jesse Armand <jesse.armand@screeningeagle.com>
2026-02-16 10:58:45 +0800

    chore(lua): Ensure conform is using stylua
    Instead of `lua_ls` for formatting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,15 +1,26 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        servers = {
-            jsonls = {
-                settings = {
-                    json = {
-                        validate = { enable = true },
-                        format = { enable = false }, -- biome handles formatting
-                    },
-                },
-            },
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    formatting = {
+      disabled = {
+        "lua_ls",
+      },
     },
+    servers = {
+      lua_ls = {
+        Lua = {
+          validate = { enable = true },
+          format = { enable = false },
+        },
+      },
+      jsonls = {
+        settings = {
+          json = {
+            validate = { enable = true },
+            format = { enable = false }, -- biome handles formatting
+          },
+        },
+      },
+    },
+  },
 }
