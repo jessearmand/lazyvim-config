78bd2d52c704593b31266ac046af572bc8ec22e2
Jesse Armand <jesse@jessearmand.com>
2026-04-04 01:13:19 +0800

    fix(plugins): Disable venv-selector
    Unnecessary for python project, remove fd dependency

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,35 +1,36 @@
-local HOME = os.getenv("HOME")
 return {
-    {
-        "oxfist/night-owl.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            require("night-owl").setup()
-            vim.cmd.colorscheme("night-owl")
+  -- Disable venv-selector: requires fd binary, unnecessary with ty + ruff
+  { "linux-cultist/venv-selector.nvim", enabled = false },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
+  },
+  {
+    "doums/rg.nvim",
+    cmd = { "Rg", "Rgf", "Rgp", "Rgfp" },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
         end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
     },
-    {
-        "doums/rg.nvim",
-        cmd = { "Rg", "Rgf", "Rgp", "Rgfp" },
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    },
+  },
 }
